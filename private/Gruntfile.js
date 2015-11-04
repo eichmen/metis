617a924128b4e8099e7bc0740e568d02cef2cf21
Pablo Pérez <pppablo83@gmail.com>
2015-11-04 18:24:16 +0200

    More changes in the multilingual support

--- private/Gruntfile.js
+++ private/Gruntfile.js
@@ -2,17 +2,26 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    nggettext_extract: {
+    'nggettext_extract': {
       pot: {
         files: {
-          'po/template.pot': ['../client/templates/*.html']
+          'po/template.pot': ['../client/templates/*.html',
+                              'langkeys.js']
         }
       },
     },
-  })
+        'nggettext_compile': {
+            all: {
+                files: {
+                    '../lib/translations.js': ['po/*.po']
+                }
+            }
+        },
+    });
 
   grunt.loadNpmTasks('grunt-angular-gettext');
 
   grunt.registerTask('default', ['nggettext_extract']);
-
+  grunt.registerTask('extract', ['nggettext_extract']);
+  grunt.registerTask('compile', ['nggettext_compile']);
 };

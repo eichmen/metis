8a7f5ffc222974bd1114d28b8957c6062e998ba0
Pablo Pérez <pppablo83@gmail.com>
2015-11-25 18:01:18 +0200

    Added in subscription the fields to which to subscribe

--- client/scripts/controllers/ingredients/ingredients.controller.js
+++ client/scripts/controllers/ingredients/ingredients.controller.js
@@ -31,12 +31,17 @@ function IngredientsCtrl ($scope,$meteor,$state) {
 
     $scope.$meteorAutorun(function() {
 
-
         console.log('Begin');
         console.log($scope.getReactively('perPage'));
         console.log($scope.getReactively('page'));
 
         $scope.$meteorSubscribe('ingredients', {
+                fields: {'nomenclature.english.shrtDesc': 1,
+                  'nomenclature.english.foodGroup': 1,
+                  'proximates.energKcal.value': 1,
+                  'proximates.protein.value': 1,
+                  'proximates.lipidTot.value': 1,
+                  'proximates.carbohydrt.value': 1},
                 limit: parseInt($scope.getReactively('perPage')),
                 skip: (parseInt($scope.getReactively('page')) - 1) * parseInt($scope.getReactively('perPage')),
                 sort: $scope.getReactively('sort')

9c2c00930f95ec46ed4b4340d759d87cd577d42c
Martín Pérez <mpbrenlla@gmail.com>
2015-12-09 18:54:35 +0100

    Improvements in theme

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -12,21 +12,10 @@ angular
         'ngMessages'
     ])
     .config(function ($mdThemingProvider) {
-
-        var customBlueMap = $mdThemingProvider.extendPalette('teal', {
-            'contrastDefaultColor': 'light',
-            'contrastDarkColors': ['50'],
-            '50': 'ffffff'
-        });
-        $mdThemingProvider.definePalette('customBlue', customBlueMap);
-        $mdThemingProvider.theme('default')
-            .primaryPalette('customBlue', {
-                'default': '500',
-                'hue-1': '50'
-            })
-            .accentPalette('pink');
-        $mdThemingProvider.theme('input', 'default')
-            .primaryPalette('grey')
+             $mdThemingProvider.theme('default')
+    .primaryPalette('blue')
+    .accentPalette('pink');
+        
     }).run(function ($rootScope, $state,gettextCatalog) {
         $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
             // We can catch the error thrown when the $requireUser promise is rejected

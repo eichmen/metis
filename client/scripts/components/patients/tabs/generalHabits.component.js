e2dd7edb735505d70ff4e5f8f1370107d7dc99f2
Martín Pérez <mpbrenlla@gmail.com>
2016-01-11 20:08:29 +0100

    Patients, added new tabs

--- client/scripts/components/patients/tabs/generalHabits.component.js
+++ client/scripts/components/patients/tabs/generalHabits.component.js
@@ -1,33 +1,37 @@
-angular.module('Metis').directive('patientGeneral', function () {
+angular.module('Metis').directive('patientGeneralHabits', function () {
     return {
         restrict: 'E',
-        templateUrl: 'client/scripts/components/patients/tabs/general.html',
-        controllerAs: 'patientGeneral',
-        controller: function ($scope, $reactive, $stateParams,$mdDialog, $mdToast) {
+        templateUrl: 'client/scripts/components/patients/tabs/generalHabits.html',
+        controllerAs: 'patientGeneralHabits',
+        controller: function ($scope, $reactive, $stateParams, $mdDialog, $mdToast) {
             $reactive(this).attach($scope);
 
             this.patientId = $stateParams.patientId;
 
             this.subscribe('patient-details', () => {
-                return [this.getReactively('patientId')];
-            });
+                    return [this.getReactively('patientId')];
+                }
+            )
+            ;
 
             this.helpers({
-                patient: () => {
-                    return Patients.findOne(this.getReactively('patientId'));
+                    patient: () => {
+                        return Patients.findOne(this.getReactively('patientId'));
+                    }
                 }
-            });
+            )
+            ;
 
-            $scope.genderList=['','Man','Woman'];
-            $scope.children=['0','1','2','3','+3'];
-            $scope.workShift=['Student','Labor inactivity','Housewife','Full-time daily','Retired','Part time'];
-            $scope.workType=['Sitting','Sitting and standing','Standing','In movement','Physically demanding work'];
-
+            $scope.smoker = ['No', 'Passive smoker', 'Weekend smoker','Some cigarettes at week','<1 pack per day','1 or more pack per day'];
+            $scope.physicalActivity = ['Very light','Light','Moderate','Intense','Very intense'];
+            $scope.pregnant = ['','Yes','No'];
+            $scope.breadFeed = ['','Yes','No'];
+            $scope.tired = ['','Yes','No'];
             this.update = update;
 
             function update() {
 
-                Meteor.call('updatePatientGeneral', this.patient, Meteor.userId(), function (error, result) {
+                Meteor.call('updatePatientGeneralHabits', this.patient, Meteor.userId(), function (error, result) {
                     if (error) {
                         console.log('failed', err);
                     } else {
@@ -50,6 +54,7 @@ angular.module('Metis').directive('patientGeneral', function () {
                         alert = undefined;
                     });
             }
+
             function toast(message) {
                 $mdToast.show(
                     $mdToast.simple()
@@ -60,4 +65,5 @@ angular.module('Metis').directive('patientGeneral', function () {
             }
         }
     }
-});
+})
+;
